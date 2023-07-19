f926a1483f96188f1488c34dfb13e5a191c9786e
Hao Lee <haolee03@hotmail.com>
2023-07-19 17:58:11 -0400

    formatting

--- inputGen.py
+++ inputGen.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser, RawTextHelpFormatter as RT
 from joblib   import Parallel, delayed
 from os       import cpu_count as ncpu
 from pathlib  import Path
-from shutil import copy2 as cp
+from shutil   import copy2 as cp
 
 # import from personal modules 
 from utils.params import getParams
@@ -26,10 +26,10 @@ def getArguments():
     parser.add_argument('n',         type = int, help="the number of parameter samples, each of which leads to a xml file")
     parser.add_argument('ipt',       type = str, help="the file path to a .yaml/yml parameter map in the parameters folder")
     parser.add_argument('tpl_xml',   type = str, help="the file path to a xml template in the template folder")
-    parser.add_argument('-tpl_pflo', type = str, help="the file path to a xml template in the template folder", default="")
+    parser.add_argument('-tpl_pflo', type = str, help="the file path to a xml template in the template folder",                         default ="")
     parser.add_argument('opt',       type = str, help="the directory to store xml files")
-    parser.add_argument('-ox', type=str, help="the directory including any other files that need to be copied to opt directory", default="")
-    parser.add_argument('-s',        type = int, help="the seed to set up the samples from the parameter space", default=960108)
+    parser.add_argument('-ox',       type =str, help="the directory including any other files that need to be copied to opt directory", default ="")
+    parser.add_argument('-s',        type = int, help="the seed to set up the samples from the parameter space",                        default =960108)
 
     return parser.parse_args()
 
@@ -42,7 +42,7 @@ def main():
     tpl_xml  = DIR.joinpath(args.tpl_xml)
     tpl_pflo = DIR.joinpath(args.tpl_pflo) if len(args.tpl_pflo) != 0 else None
     opt      = DIR.joinpath(args.opt)
-    other = DIR.joinpath(args.ox) if len(args.ox) != 0 else None
+    other    = DIR.joinpath(args.ox)       if len(args.ox) != 0       else None
 
     # sample from the parameter space 
     # params is a list of parameter maps
