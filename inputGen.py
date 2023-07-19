66cdfb9fe19a4809fad53bff7d813136d1819599
Hao Lee <hleeh@br014.ib.bridges2.psc.edu>
2023-07-19 17:55:00 -0400

    add 'others' feature

--- inputGen.py
+++ inputGen.py
@@ -1,8 +1,9 @@
 # import from built-in modules 
 from argparse import ArgumentParser, RawTextHelpFormatter as RT
+from joblib   import Parallel, delayed
 from os       import cpu_count as ncpu
 from pathlib  import Path
-from joblib   import Parallel, delayed
+from shutil import copy2 as cp
 
 # import from personal modules 
 from utils.params import getParams
@@ -27,6 +28,7 @@ def getArguments():
     parser.add_argument('tpl_xml',   type = str, help="the file path to a xml template in the template folder")
     parser.add_argument('-tpl_pflo', type = str, help="the file path to a xml template in the template folder", default="")
     parser.add_argument('opt',       type = str, help="the directory to store xml files")
+    parser.add_argument('-ox', type=str, help="the directory including any other files that need to be copied to opt directory", default="")
     parser.add_argument('-s',        type = int, help="the seed to set up the samples from the parameter space", default=960108)
 
     return parser.parse_args()
@@ -40,6 +42,7 @@ def main():
     tpl_xml  = DIR.joinpath(args.tpl_xml)
     tpl_pflo = DIR.joinpath(args.tpl_pflo) if len(args.tpl_pflo) != 0 else None
     opt      = DIR.joinpath(args.opt)
+    other = DIR.joinpath(args.ox) if len(args.ox) != 0 else None
 
     # sample from the parameter space 
     # params is a list of parameter maps
@@ -53,6 +56,12 @@ def main():
                 )
             for i in range(n)
         )
+
+    # add other required files to out directory 
+    if args.ox is not None:
+        files = Path(other).glob(*)
+        for f in files:
+            cp(f, opt)
 
 if __name__ == "__main__":
     main()
